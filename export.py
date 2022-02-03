9facdf7ebce819dbc9e7538975f100d51e0f2495
jooyoung jang <jyjang1090@snuailab.com>
2022-02-03 14:50:09 +0900

    add .eval

--- export.py
+++ export.py
@@ -7,6 +7,7 @@ def export():
     detector_onnx_save_path = '/workspace/LPD-end-to-end/CCPD_carplate_bbox_weights_12345789_1ssd512_25000.onnx'
     net = build_ssd('export', 512, 2)
     net.load_weights(weight_path)
+    net.eval()
     dummy_input = torch.rand((1, 3, 512, 512))
     torch.onnx.export(net, dummy_input, detector_onnx_save_path, input_names=['input'],
                               output_names=['output'])
